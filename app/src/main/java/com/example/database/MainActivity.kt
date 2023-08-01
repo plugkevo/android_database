40539814f4c5fc1daa9afafc9b49f13d305a27a2
plugkevo <munenekevin366@gmail.com>
2023-08-01 16:33:56 +0300

    php code done

--- app/src/main/java/com/example/database/MainActivity.kt
+++ app/src/main/java/com/example/database/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.database
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mybtn = findViewById<TextView>(R.id.to_signup2)
+        mybtn.setOnClickListener {
+            val intent=Intent(this, sign_up::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/database/sign_up.kt
+++ app/src/main/java/com/example/database/sign_up.kt
@@ -1,11 +1,31 @@
 package com.example.database
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class sign_up : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val mybtn = findViewById<TextView>(R.id.to_login)
+        mybtn.setOnClickListener {
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        var name = findViewById<TextView>(R.id.name)
+        var email = findViewById<TextView>(R.id.email)
+        var contact = findViewById<TextView>(R.id.contact)
+        var password = findViewById<TextView>(R.id.password)
+        var button = findViewById<Button>(R.id.btn_signup)
+        button.setOnClickListener {
+
+        }
     }
-}
+    }
