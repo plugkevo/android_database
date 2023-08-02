4d452dd119a7e820769980cad4007696d86be242
plugkevo <munenekevin366@gmail.com>
2023-08-02 16:09:09 +0300

    php code done

--- app/src/main/java/com/example/database/sign_up.kt
+++ app/src/main/java/com/example/database/sign_up.kt
@@ -6,6 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class sign_up : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -26,6 +33,17 @@ class sign_up : AppCompatActivity() {
         var button = findViewById<Button>(R.id.btn_signup)
         button.setOnClickListener {
 
+            var url:String= "http://192.168.100.191/android_class/add_student.php?name="+ name.text.toString() +email.text.toString() +contact.text.toString() +password.text.toString()
+
+            var rgst:RequestQueue = Volley.newRequestQueue(this )
+
+            val jos = JsonObjectRequest(Request.Method.GET,url,null, {
+                    response -> Toast.makeText(this, "Record added successfully", Toast.LENGTH_LONG).show()}, { error-> })
+
+            rgst.add(jos)
+            }
+
+
         }
     }
-    }
+
